1f70fface9cf5ee90cffe24b46003c2ecba18a5a
Kimani <elvis.kimani@student.moringaschool.com>
2022-06-14 16:02:32 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    createToDo(e.target.new_task_description.value)
+    form.reset()
+  })
+  
 });
+ function createToDo(todo){
+   let li = document.createElement('li');
+   let deletebtn = document.createElement('button');
+   deletebtn.addEventListener('click',deleteTodo)
+   deletebtn.textContent = 'done'
+   li.textContent = `${todo} `;
+   li.appendChild(deletebtn);
+   console.log(li)
+  
+   document.querySelector('#tasks').appendChild(li)
+ }
+  function deleteTodo(e){
+    e.target.parentNode.remove()
+  }
